34f41c6d3917a43469ea76d2ebf36bb5f226605d
Tyler Arehart <tarehart@gmail.com>
2020-05-24 12:21:35 -0700

    Rearranging folders.

--- generate-action-swagger.bat
+++ generate-action-swagger.bat
@@ -1,11 +1,11 @@
 java -jar swagger-codegen-cli-3.0.20.jar generate^
-   -i swagger.yaml^
+   -i action-swagger.yaml^
    -l python-flask^
-   -o src\server^
+   -o src\action-server^
    -DpackageName=rlbot_action_server
 
 java -jar swagger-codegen-cli-3.0.20.jar generate^
-   -i swagger.yaml^
+   -i action-swagger.yaml^
    -l python^
-   -o src\client^
+   -o src\action-client^
    -DpackageName=rlbot_action_client
