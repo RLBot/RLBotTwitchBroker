915732fd63d58c93212a4a441951da3641b4e63a
Tyler Arehart <tarehart@gmail.com>
2020-05-24 12:49:26 -0700

    Basic implementation of register controller.

--- src/broker-server/rlbot_twitch_broker_server/controllers/register_controller.py
+++ src/broker-server/rlbot_twitch_broker_server/controllers/register_controller.py
@@ -5,17 +5,10 @@ from rlbot_twitch_broker_server.models.action_server_registration import ActionS
 from rlbot_twitch_broker_server.models.api_response import ApiResponse  # noqa: E501
 from rlbot_twitch_broker_server import util
 
+KNOWN_ACTION_SERVERS = {}
 
 def register_action_server(body):  # noqa: E501
-    """register_action_server
-
-     # noqa: E501
-
-    :param body: 
-    :type body: dict | bytes
-
-    :rtype: ApiResponse
-    """
-    if connexion.request.is_json:
-        body = ActionServerRegistration.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+    body = ActionServerRegistration.from_dict(connexion.request.get_json())  # noqa: E501
+    server_key = connexion.request.remote_addr()
+    KNOWN_ACTION_SERVERS[server_key] = body
+    return ApiResponse(200, f"Successfully registered {server_key}.").to_dict()
