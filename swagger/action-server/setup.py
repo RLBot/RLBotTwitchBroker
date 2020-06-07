a1e1a5336c14e8cf525f49be39024d6710859456
Jiří Pihrt <jira.pihrt@volny.cz>
2020-06-08 01:42:56 +0200

    Let scripts handle the player name highlighting

--- swagger/action-server/setup.py
+++ swagger/action-server/setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup, find_packages
 
 NAME = "rlbot_action_server"
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 # To install the library, run the following
 #
 # python setup.py install
